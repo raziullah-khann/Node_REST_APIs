f5797ec9d0a713b549fa94e4b512ce27886f3675
raziullah-khann <raziullahkhan25@gmail.com>
2024-12-19 02:18:49 +0530

    Sending the request and response and Working with Postman

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const feedRoute = require("./routes/feed");
 
 const app = express();
 
+// app.use(bodyParser.urlencoded({extended: false})); //x-www-form-urlencoded <form>
+// Middleware to parse JSON bodies
+app.use(bodyParser.json()); //application/json
+
 app.use("/feed", feedRoute)
 
-app.listen(8080)
+app.listen(8080, () => {
+    console.log("Server is running on http://localhost:8080");
+})
