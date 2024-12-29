105ceb4e1d9a54480d296da75a4dbb55e2a2ee9f
raziullah-khann <raziullahkhan25@gmail.com>
2024-12-29 21:30:40 +0530

    fetching list of posts from backend to frontend

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,29 @@
-exports.getPost = (req,res, next) => {
-    res.status(200).json({
-        posts: [{title: 'First Post', content: 'This is the first post!'}]
-    });
+const path = require("path");
+
+exports.getPost = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1', 
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl:"images/duck.jpg",
+        creator: {
+            name: 'Raziullah'
+        },
+        createdAt: new Date()
+      },
+    ],
+  });
 };
 
 exports.createPost = (req, res, next) => {
-    //suppose we get title and content from the body
-    const {title, content} = req.body;
-    console.log(title, content);
-    //create post in db
-    res.status(201).json({
-        message: "Post created successfully!",
-        post: {id: new Date().toISOString(), title: title, content: content}
-    })
-}
+  //suppose we get title and content from the body
+  const { title, content } = req.body;
+  console.log(title, content);
+  //create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
