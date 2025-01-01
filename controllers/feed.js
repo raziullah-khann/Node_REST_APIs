508401457ffb4bc40a09b307093d2d4d20a3a05e
raziullah-khann <raziullahkhan25@gmail.com>
2025-01-01 15:58:14 +0530

    Storing posts in te database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,34 +1,52 @@
 const path = require("path");
-const {validationResult} = require('express-validator');
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPost = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: '1', 
+        _id: "1",
         title: "First Post",
         content: "This is the first post!",
-        imageUrl:"images/duck.jpg",
+        imageUrl: "images/duck.jpg",
         creator: {
-            name: 'Raziullah'
+          name: "Raziullah",
         },
-        createdAt: new Date()
+        createdAt: new Date(),
       },
     ],
   });
 };
 
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({message: 'Validation failed, entered data is incorrect', errors: errors.array()})
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
   //suppose we get title and content from the body
   const { title, content } = req.body;
   console.log(title, content);
   //create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { _id: new Date().toISOString(), title: title, content: content, creator: {name: 'Raziullah'}, createdAt: new Date() },
+  const post = new Post({
+    title: title,
+    imageUrl: 'images/duck.jpg',
+    content: content,
+    creator: { name: "Raziullah" },
   });
+  post
+    .save()
+    .then((post) => {
+      console.log(post);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: post,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
