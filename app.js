4fe9657912d0c5b4a3ca056077d6b5d14fcf1d9a
raziullah-khann <raziullahkhan25@gmail.com>
2025-01-01 15:02:29 +0530

    Setting up a post schema and  model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const feedRoute = require("./routes/feed");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
@@ -10,14 +11,26 @@ const app = express();
 app.use(bodyParser.json()); //application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use("/feed", feedRoute)
+app.use("/feed", feedRoute);
 
-app.listen(8080, () => {
-    console.log("Server is running on http://localhost:8080");
-})
+mongoose
+  .connect(
+    "mongodb+srv://Raziullah-Khan:AXLIVFo3hpQp1jRF@cluster0.frgxn.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(result => {
+      app.listen(8080, () => {
+        console.log("Server is running on http://localhost:8080");
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {check, body} = require("express-validator");
 
 const feedController = require("../controllers/feed");
 
@@ -8,6 +9,6 @@ const router = express.Router(); //creates a lightweight, modular route handler.
 router.get("/posts", feedController.getPost);
 
 // POST /feed/posts
-router.post("/post", feedController.createPost);
+router.post("/post", [body('title').trim().isLength({min:5}), body('content').trim().isLength({min:5})], feedController.createPost);
 
 module.exports = router;
