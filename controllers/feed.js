14cba5486bf4034af2bec2c4cccdd5db30cd8a81
raziullah-khann <raziullahkhan25@gmail.com>
2025-01-03 22:24:23 +0530

    Uploading images in form handle it with multer

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,27 +14,37 @@ exports.getPost = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const image = req.file;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed, entered data is incorrect");
     error.statusCode = 422;
     throw error;
   }
+
+  if(!image){
+    const error = new Error('No image provide!');
+    error.statusCode = 422;
+    throw error;
+  }
+  console.log("image object hai create karte time ka",image);
+  const imageUrl = image.path;
+
   //suppose we get title and content from the body
   const { title, content } = req.body;
   console.log(title, content);
   //create post in db
   const post = new Post({
     title: title,
-    imageUrl: "images/duck.jpg",
-    // imageUrl: `${req.protocol}://${req.get("host")}/images/duck.jpg`, // Generates full URL
+    // imageUrl: 'images/'+imageUrl,
+    imageUrl: imageUrl,
     content: content,
     creator: { name: "Raziullah" },
   });
   post
     .save()
     .then((post) => {
-      console.log(post);
+      console.log("Backend post object:", post);
       res.status(201).json({
         message: "Post created successfully!",
         post: post,
@@ -51,14 +61,14 @@ exports.createPost = (req, res, next) => {
 exports.getSinglePost = (req, res, next) => {
   const postId = req.params.postId;
   //fetching from database
-  Post.find({ _id: postId })
+  Post.findById(postId)
     .then((post) => {
       if (!post) {
         const error = new Error("Could not find post.");
-        error.statusCode = 400;
+        error.statusCode = 404;
         throw error;
       }
-      console.log(post);
+      console.log("single post fetch krte time",post);
       res.status(200).json({message: 'Post fetched.', post: post})
     })
     .catch((err) => {
