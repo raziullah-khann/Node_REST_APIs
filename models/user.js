6777f5a2ef19a7053eccb3c01a538a53e594983e
raziullah-khann <raziullahkhan25@gmail.com>
2025-01-05 22:25:44 +0530

    Adding a user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
+});
+
+module.exports = mongoose.model('user', userSchema);
+
