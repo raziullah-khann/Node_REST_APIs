f1ffffe8887d3a6bd50b7c383b03da870c6f4260
raziullah-khann <raziullahkhan25@gmail.com>
2025-01-10 20:24:38 +0530

    Transform all .then() and .catch() code in async await

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed!");
@@ -19,64 +19,55 @@ exports.signup = (req, res, next) => {
   const name = req.body.name;
   const password = req.body.password;
 
-  bcrypt
-    .hash(password, 12)
-    .then((hashPassword) => {
-      console.log("hash pass is ", hashPassword);
-      const user = new User({
-        email: email,
-        password: hashPassword,
-        name: name,
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({ message: "User created!", userId: result._id });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const hashPassword = await bcrypt.hash(password, 12);
+    console.log("hash pass is ", hashPassword);
+    const user = new User({
+      email: email,
+      password: hashPassword,
+      name: name,
     });
+    const result = await user.save();
+    res.status(201).json({ message: "User created!", userId: result._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const { email, password } = req.body;
-  let loadedUser; //store user in outside of findOne to manage scoping and ensure access to user data across promise chains.  
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        const error = new Error("A user with this email could not be found!");
-        error.statusCode = 401;
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((isEqual) => {
-      if (!isEqual) {
-        const error = new Error("Wrong password!");
-        error.statusCode = 401;
-        throw error;
-      }
-      const token = jwt.sign(
-        { email: loadedUser.email, userId: loadedUser._id.toString() },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
-      res
-        .status(200)
-        .json({
-          message: "Authenticate Successfully!",
-          token: token,
-          userId: loadedUser._id.toString(),
-        });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  let loadedUser; //store user in outside of findOne to manage scoping and ensure access to user data across promise chains.
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("A user with this email could not be found!");
+      error.statusCode = 401;
+      throw error;
+    }
+    loadedUser = user;
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Wrong password!");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      { email: loadedUser.email, userId: loadedUser._id.toString() },
+      SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: "Authenticate Successfully!",
+      token: token,
+      userId: loadedUser._id.toString(),
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
